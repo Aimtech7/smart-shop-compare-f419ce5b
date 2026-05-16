e2813b2062eb4983da0eeeb9a7cac5f0e2ac5e69
Aimtech7 <austinemakwaka254@gmail.com>
2026-05-17 02:42:37 +0300

    refactor: normalize wishlist service with product mapper

--- src/services/django/wishlist.ts
+++ src/services/django/wishlist.ts
@@ -1,10 +1,12 @@
 import { http } from './client';
+import { mappers } from './mappers';
 import type { Product } from '@/types';
 
 export const djangoWishlist = {
   get: async () => {
     const res = await http.get<any>('/wishlist/');
-    return res.data || res.items || res || [];
+    const data = res.data || res.items || res || [];
+    return data.map((item: any) => mappers.product(item.product || item));
   },
   
   add: (productId: string) => 
